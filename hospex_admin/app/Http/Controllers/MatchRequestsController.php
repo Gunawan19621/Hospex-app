5f161e1ae4ffdbb932131b0f4e4f2df984dc3fbc
lulumuhamad <lulumuhamad01@gmail.com>
2020-08-11 13:36:55 +0700

    list business match tabs

--- hospex_admin/app/Http/Controllers/MatchRequestsController.php
+++ hospex_admin/app/Http/Controllers/MatchRequestsController.php
@@ -39,23 +39,30 @@ class MatchRequestsController extends Controller
         // }
         // return $data;
         if(request()->ajax()){
-            return datatables()->of(Match::all())
+            return datatables()->of(Match::where(['status' => '1'])->get())
                     ->addIndexColumn()
+                    ->addColumn('event', function($data){  return  $data->exhibitor->event->event_title.' - ' . $data->exhibitor->event->year; })
                     ->addColumn('date', function($data){  return  $data->availableschedule->date; })
                     ->addColumn('time', function($data){  return  $data->availableschedule->time; })
                     ->editColumn('status', function(Match $match) {
-                        return ( $match->status == 0 || $match->status_visitor == 0? 
+                        return ( $match->status_meeting == 0? 
                             // $this->viewForm($match->id)
                             ' <a href="javascript::void(0);" class="btn btn-sm btn-outline-success m-btn m-btn--icon m-btn--pill" >
-                                <span><i class="fa fa-calendar-check-o"></i><span>Approve</span></span>
+                                <span><i class="fa fa-calendar-check-o"></i><span>Unconfirm</span></span>
                             </a>'
                             : 
                             '<a href="#" class="btn btn-sm btn-success m-btn m-btn--icon m-btn--pill">
-                                <span> <i class="fa fa-calendar-check-o"></i><span>Approved</span></span>
+                                <span> <i class="fa fa-calendar-check-o"></i><span>Confirm</span></span>
                             </a>');
                     })
                     ->addColumn('visitor_name', function($data){
                         return $data->visitor->visitor_name;
+                    })
+                    ->addColumn('visitor_company', function($data){
+                        return $data->visitor->company;
+                    })
+                    ->addColumn('visitor_email', function($data){
+                        return $data->visitor->visitor_email;
                     })
                     ->addColumn('exhibitor_name', function($data){
                         return $data->exhibitor->company->company_name;
@@ -76,7 +83,53 @@ class MatchRequestsController extends Controller
         }
         return view('match.index',compact('title'));
     }
-
+    public function pendingMatch()
+    {
+        if(request()->ajax()){
+            return datatables()->of(Match::whereIn('status',['0','2'])->get())
+                    ->addIndexColumn()
+                    ->addColumn('event', function($data){  return  $data->exhibitor->event->event_title.' - ' . $data->exhibitor->event->year; })
+                    ->addColumn('date', function($data){  return  $data->availableschedule->date; })
+                    ->addColumn('time', function($data){  return  $data->availableschedule->time; })
+                    ->editColumn('status', function(Match $match) {
+                        return ( $match->status == 0? 
+                            // $this->viewForm($match->id)
+                            ' <a href="javascript::void(0);" class="btn btn-sm btn-outline-success m-btn m-btn--icon m-btn--pill" >
+                                <span><i class="fa fa-calendar-check-o"></i><span>Pending</span></span>
+                            </a>'
+                            : 
+                            '<a href="#" class="btn btn-sm btn-success m-btn m-btn--icon m-btn--pill">
+                                <span> <i class="fa fa-calendar-check-o"></i><span>Decline</span></span>
+                            </a>');
+                    })
+                    ->addColumn('visitor_name', function($data){
+                        return $data->visitor->visitor_name;
+                    })
+                    ->addColumn('visitor_company', function($data){
+                        return $data->visitor->company;
+                    })
+                    ->addColumn('visitor_email', function($data){
+                        return $data->visitor->visitor_email;
+                    })
+                    ->addColumn('exhibitor_name', function($data){
+                        return $data->exhibitor->company->company_name;
+                    })
+                    // ->addColumn('action', function($data){
+                    //     $button = '<span class="dropdown">
+                    //     <a href="#" class="btn m-btn m-btn--hover-brand m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="dropdown" aria-expanded="true"><i class="la la-ellipsis-h"></i></a> 
+                    //         <div class="dropdown-menu dropdown-menu-right">       
+                    //             <a class="dropdown-item" href="'.url('visitors/'.$data['id'].'/edit').'"><i class="la la-edit"></i> Edit</a>        
+                    //             <a class="dropdown-item" href="#"><i class="la la-trash"></i> Hapus</a>        
+                    //         </div>
+                    //     </span>';
+                    //     // $button .= '<a href="{{ url('events/$data->id}') }}" class="m-portlet__nav-link btn m-btn m-btn--hover-brand m-btn--icon m-btn--icon-only m-btn--pill" title="View">  <i class="la la-edit"></i></a>`;
+                    //     return $button;
+                    // })
+                ->rawColumns(['visitor_name','exhibitor_name','status'])
+                ->make(true);
+        }
+        
+    }
     /**
      * Show the form for creating a new resource.
      *
--- hospex_admin/resources/views/match/script.blade.php
+++ hospex_admin/resources/views/match/script.blade.php
@@ -2,42 +2,114 @@
 <script>
     $(document).ready(function(){
         alertMessage(flash)
-        var table = $('#m_table_1').DataTable({
-            processing : true,
-            serverSide  : true,
-            ajax : {
-                url : "{{ route('matches.index') }}",
-            },
-            columns:[
-                {
-                    data: 'DT_RowIndex',
-                    orderable: false, 
-                    searchable: false
-                },
-                {
-                    data : "visitor_name", name : "visitor_name",
-                },
-                {
-                    data : "exhibitor_name", name : "exhibitor_name",
-                },
-                {
-                    data : "date", name : "date",
-                },
-                {
-                    data : "time", name : "time",
-                },
-                {
-                    data : "status", name : "status",
-                },
-                // {
-                //     data:"action",
-                //     name: "action",
-                //     orderable: false
-                // }
-            ]
-        })
-       
+        approve()
     })
+    function approve(){
+        if (!$.fn.dataTable.isDataTable('#approveTable')) {
+            $('#approveTable').DataTable({
+                dom: 'Bfrtip',
+                processing: true,
+                serverSide: true,
+                order: [[0, 'desc']],
+                buttons: [
+                    'csv', 'excel', 'pdf', 'print'
+                ],
+                ajax : {
+                        url : "{{ route('matches.index') }}",
+                    },
+                columns:[
+                        {
+                            data: 'DT_RowIndex',
+                            orderable: false, 
+                            searchable: false
+                        },
+                        {
+                            data : "event", name : "event",
+                        },
+                        {
+                            data : "visitor_name", name : "visitor_name",
+                        },
+                        {
+                            data : "visitor_company", name : "visitor_company",
+                        },
+                        {
+                            data : "visitor_email", name : "visitor_email",
+                        },
+                        {
+                            data : "exhibitor_name", name : "exhibitor_name",
+                        },
+                        {
+                            data : "date", name : "date",
+                        },
+                        {
+                            data : "time", name : "time",
+                        },
+                        {
+                            data : "status", name : "status",
+                        },
+                        // {
+                        //     data:"action",
+                        //     name: "action",
+                        //     orderable: false
+                        // }
+                    ],
+                order: [[0, 'desc']]
+                });
+        }
+    }
+    function pending(){
+        if (!$.fn.dataTable.isDataTable('#pendingTable')) {
+            $('#pendingTable').DataTable({
+                dom: 'Bfrtip',
+                processing: true,
+                serverSide: true,
+                order: [[0, 'desc']],
+                buttons: [
+                    'csv', 'excel', 'pdf', 'print'
+                ],
+                ajax : {
+                        url : "{{ route('matches.pending') }}",
+                    },
+                columns:[
+                        {
+                            data: 'DT_RowIndex',
+                            orderable: false, 
+                            searchable: false
+                        },
+                        {
+                            data : "event", name : "event",
+                        },
+                        {
+                            data : "visitor_name", name : "visitor_name",
+                        },
+                        {
+                            data : "visitor_company", name : "visitor_company",
+                        },
+                        {
+                            data : "visitor_email", name : "visitor_email",
+                        },
+                        {
+                            data : "exhibitor_name", name : "exhibitor_name",
+                        },
+                        {
+                            data : "date", name : "date",
+                        },
+                        {
+                            data : "time", name : "time",
+                        },
+                        {
+                            data : "status", name : "status",
+                        },
+                        // {
+                        //     data:"action",
+                        //     name: "action",
+                        //     orderable: false
+                        // }
+                    ],
+                order: [[0, 'desc']]
+                });
+        }
+    }
 //    function approve(event,id){
 //     //    console.log(id)
 //     //    console.log(event)
