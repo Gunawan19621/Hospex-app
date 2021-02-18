95f5e4d975a188f341f81d2710c4ff43df01c7f3
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-02-18 10:53:49 +0700

    no message

--- hospex_admin/routes/web.php
+++ hospex_admin/routes/web.php
@@ -20,71 +20,72 @@ use Illuminate\Support\Str;
 Auth::routes();
 
 // Events
-Route::get('/events','EventController@index');
-Route::post('/events','EventController@store');
-Route::get('/events/getevents', 'EventController@getevents');
-Route::get('/events/create','EventController@create');
-Route::get('/events/{event}','EventController@show');
-Route::delete('/events/{event}','EventController@destroy');
-Route::get('/events/{event}/edit','EventController@edit');
-Route::get('/events/{event}/exhibitor','EventController@exhibitor');
-Route::get('/events/{event}/stand','EventController@stand');
-Route::get('/events/{event}/area','EventController@area');
-Route::get('/events/{event}/site-plan','EventController@siteplan');
-Route::get('/events/{event}/available-schedule','EventController@availableSchedule');
-Route::get('/events/{event}/upload-site-plan','EventController@uploadSiteplan');
-Route::patch('/events/{event}/site-plan','EventController@fileStore');
-Route::get('dropzone/{event}/fetch','EventController@fetch');
-Route::get('/dropzone/delete','EventController@dropzoneDelete')->name('dropzone.delete');
+Route::group(['middleware' => ['auth']], function () { 
+    Route::get('/events','EventController@index');
+    Route::post('/events','EventController@store');
+    Route::get('/events/getevents', 'EventController@getevents');
+    Route::get('/events/create','EventController@create');
+    Route::get('/events/{event}','EventController@show');
+    Route::delete('/events/{event}','EventController@destroy');
+    Route::get('/events/{event}/edit','EventController@edit');
+    Route::get('/events/{event}/exhibitor','EventController@exhibitor');
+    Route::get('/events/{event}/stand','EventController@stand');
+    Route::get('/events/{event}/area','EventController@area');
+    Route::get('/events/{event}/site-plan','EventController@siteplan');
+    Route::get('/events/{event}/available-schedule','EventController@availableSchedule');
+    Route::get('/events/{event}/upload-site-plan','EventController@uploadSiteplan');
+    Route::patch('/events/{event}/site-plan','EventController@fileStore');
+    Route::get('dropzone/{event}/fetch','EventController@fetch');
+    Route::get('/dropzone/delete','EventController@dropzoneDelete')->name('dropzone.delete');
 
-Route::patch('/events/{event}','EventController@update');
-// yg atas adalah route default maka bisa diganti dengan yg bawah
-// Route::resource('events','EventController');
-Route::resource('eventschedules','EventSchedulesController');
-Route::get('/eventschedules/create/{event}','EventSchedulesController@create');
-Route::resource('eventrundown','EventRundownController');
-Route::get('/eventrundown/create/{schedule}','EventRundownController@create');
+    Route::patch('/events/{event}','EventController@update');
+    // yg atas adalah route default maka bisa diganti dengan yg bawah
+    // Route::resource('events','EventController');
+    Route::resource('eventschedules','EventSchedulesController');
+    Route::get('/eventschedules/create/{event}','EventSchedulesController@create');
+    Route::resource('eventrundown','EventRundownController');
+    Route::get('/eventrundown/create/{schedule}','EventRundownController@create');
 
-// Categories
-Route::resource('categories', 'CategoriesController');
+    // Categories
+    Route::resource('categories', 'CategoriesController');
 
-// Companies
-Route::resource('companies', 'CompaniesController');
+    // Companies
+    Route::resource('companies', 'CompaniesController');
 
-// Sponsors
-Route::resource('sponsors', 'EventSponsorsController');
+    // Sponsors
+    Route::resource('sponsors', 'EventSponsorsController');
 
-// Areas
-Route::get('/areas/create/{event?}','AreasController@create');
-Route::resource('areas', 'AreasController');
+    // Areas
+    Route::get('/areas/create/{event?}','AreasController@create');
+    Route::resource('areas', 'AreasController');
 
-// Exhibitors
-Route::get('exhibitors/create/{event?}','EventExhibitorsController@create');
-Route::resource('exhibitors', 'EventExhibitorsController')->except(['create']);
+    // Exhibitors
+    Route::get('exhibitors/create/{event?}','EventExhibitorsController@create');
+    Route::resource('exhibitors', 'EventExhibitorsController')->except(['create']);
 
-Route::get('available-schedule/create/{event?}','AvailableScheduleController@create');
-Route::resource('available-schedule','AvailableScheduleController')->except(['create']);
+    Route::get('available-schedule/create/{event?}','AvailableScheduleController@create');
+    Route::resource('available-schedule','AvailableScheduleController')->except(['create']);
 
-// Stand
-Route::get('/stands/create/{event?}','StandsController@create');
-Route::resource('stands', 'StandsController')->except(['create']);
+    // Stand
+    Route::get('/stands/create/{event?}','StandsController@create');
+    Route::resource('stands', 'StandsController')->except(['create']);
 
-// Visitors
-Route::resource('visitors', 'EventVisitorsController');
+    // Visitors
+    Route::resource('visitors', 'EventVisitorsController');
 
-// Matches
-Route::get('matches/pending', 'MatchRequestsController@pendingMatch')->name('matches.pending');
-Route::resource('matches', 'MatchRequestsController');
-Route::get('matches/{match}/approve', 'MatchRequestsController@approve');
+    // Matches
+    Route::get('matches/pending', 'MatchRequestsController@pendingMatch')->name('matches.pending');
+    Route::resource('matches', 'MatchRequestsController');
+    Route::get('matches/{match}/approve', 'MatchRequestsController@approve');
 
+    Route::get('home', function(){
+        return redirect('/');
+    });
+});
 
 Route::get('/', function(){
     return view('layout.content');
 })->middleware('auth');
-
-Route::get('/home', function(){
-    return route('/');
-});
 
 // excel
 Route::get('export/{event?}', 'ExhibitorExcelController@export')->name('export');
