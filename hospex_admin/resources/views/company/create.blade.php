f13b32c7e7f6bfa143511108142314995399ae1e
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-02-24 18:04:32 +0700

    push

--- hospex_admin/resources/views/company/create.blade.php
+++ hospex_admin/resources/views/company/create.blade.php
@@ -60,6 +60,7 @@
                   <div class="form-group m-form__group">
                     <label for="companyAddress">Categories</label>
                     <select class="form-control m-select2" id="m_select2_3" name="categories[]" multiple="multiple" data-select2-tag="true" autocomplete="off">
+                        <option value="">Categories</option>
                         @foreach ($categories as $category)
                         <option  value=" {{ $category->id }} " > {{ $category->category_name }} </option>
                         @endforeach
