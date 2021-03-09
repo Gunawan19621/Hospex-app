332bd9666648d30a97d7c186df859695afdf9694
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-03-09 22:17:20 +0700

    no message

--- hospex_admin/routes/api.php
+++ hospex_admin/routes/api.php
@@ -14,6 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// exhibitors
+$router->get('exhibitors', 'Api\ExhibitorsController@index');
+$router->get('exhibitors/{exhibitor}', 'Api\ExhibitorsController@show');
+
+// business matching
+$router->post('match-request', 'Api\BusinessMatchingController@store');
+$router->put('match-request/{match}/{type}', 'Api\BusinessMatchingController@update');
+$router->post('match-approve/{match}', 'Api\BusinessMatchingController@approve');
+$router->get('list-business-matching/{type}/{id}/{status}', 'Api\BusinessMatchingController@index');
+$router->get('match-success/{id}', 'Api\BusinessMatchingController@updateStatusMeeting');
+
+$router->get('matchExhibitor', 'Api\BusinessMatchingController@list_matching');
+$router->get('matchVisitor', 'Api\BusinessMatchingController@list_matching');
+
+$router->get('event', 'Api\EventController@index');
+
+// schedule
+$router->get('schedules', 'Api\SchedulesController@index');
+$router->get('schedules/{id}', 'Api\SchedulesController@show');
+
+$router->post('register', 'Api\AuthController@register');
+$router->post('login', 'Api\AuthController@login');
+$router->get('user/{id}','Api\AuthController@getUser');
+$router->post('user/change-password','Api\AuthController@changePassword');
+$router->post('user/change-profile','Api\AuthController@changeProfile');
+$router->post('image/upload','Api\ImageController@uploadImage');
+$router->get('image/logo/{exhibitor}','Api\ImageController@logo');
+
+// available schedule
+$router->get('available-schedule/{exhibitor}','Api\AvailableController@index');
