6c8f412081a907dc57f4de5460012885ff98b5c1
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-05-09 17:54:24 +0700

    no message

--- hospex_admin/app/Http/Controllers/CategoriesController.php
+++ hospex_admin/app/Http/Controllers/CategoriesController.php
@@ -21,7 +21,7 @@ class CategoriesController extends Controller
         $title = 'Categories';
         if (request()->ajax()) 
         {
-            return datatables()->of(Category::where('id','desc'))
+            return datatables()->of(Category::get())
                     ->addIndexColumn()
                     ->addColumn('action', function($data){
                             $button = '<span class="dropdown">
