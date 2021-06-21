6a2a977b698d3c0715acc1cf47ed7c9c85e4c37e
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-06-21 09:40:36 +0700

    no message

--- hospex_admin/resources/views/auth/passwords/reset_post.blade.php
+++ hospex_admin/resources/views/auth/passwords/reset_post.blade.php
@@ -7,8 +7,10 @@
         <h3 style="text-align: center;" class="m-login__title">Reset Password</h3><br>
         <h5 style="text-align: center;">{{$message}}</h5>
 		<br>
-        <h4 style="text-align: center;">Download Aplikasi Hospex di:</h4>
-    	<a style="text-align:center;" href='https://play.google.com/store/apps/details?id=id.indigital.hospex&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+        <h4 style="text-align: center;">Download Aplikasi Hospex di:
+        	<a style="text-align:center;" href='https://play.google.com/store/apps/details?id=id.indigital.hospex&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+        </h4>
+    	
     </div>
 </div>
 @endsection
