6685dcab8c7a49b31d7e1e6cd0c045cba6f9dd5e
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-06-21 09:31:30 +0700

    no message

--- hospex_admin/resources/views/auth/passwords/reset_post.blade.php
+++ hospex_admin/resources/views/auth/passwords/reset_post.blade.php
@@ -6,12 +6,13 @@
 
         <div class="m-login__wrapper-2 m-portlet-full-height">
             <div class="m-login__contanier">
-                <div class="m-login__signin">
-                    <div class="m-login__head">
-                        <h3 class="m-login__title">Reset Password</h3>
-                        {{$message}}
+                <div class="m-login__signin" style="text-align: center;">
+                    <div class="m-login__head" style="text-align: center;">
+                        <h3 class="m-login__title">Reset Password</h3><br>
+                        <p style="text-align: center;">{{$message}}</p>
                     </div>
                     
+                    Download Aplikasi Hospex di:<br>
                     <a href='https://play.google.com/store/apps/details?id=id.indigital.hospex&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
                 </div>
             </div>
