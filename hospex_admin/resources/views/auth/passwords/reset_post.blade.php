bfb64af7056d79e686ae20d113256d9883809341
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-06-21 09:29:01 +0700

    no message

--- hospex_admin/resources/views/auth/passwords/reset_post.blade.php
+++ hospex_admin/resources/views/auth/passwords/reset_post.blade.php
@@ -9,9 +9,8 @@
                 <div class="m-login__signin">
                     <div class="m-login__head">
                         <h3 class="m-login__title">Reset Password</h3>
+                        {{$message}}
                     </div>
-                        
-                    {{$message}}
                     
                     <a href='https://play.google.com/store/apps/details?id=id.indigital.hospex&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
                 </div>
