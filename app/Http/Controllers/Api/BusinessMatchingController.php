f947d273d21d4bca3fdd43402b757569d5e077f2
Mikhael Bagus <bagus.mikhael@gmail.com>
2021-08-09 10:02:35 +0700

    Update BusinessMatchingController.php

--- app/Http/Controllers/Api/BusinessMatchingController.php
+++ app/Http/Controllers/Api/BusinessMatchingController.php
@@ -270,13 +270,13 @@ class BusinessMatchingController extends Controller
                         if($request->reason == null || $request->reason == ''){
                             $notification = [
                                 'title' => 'Business Matching Confirm',
-                                'body'  => 'Your request confirmed by '.$eventExhibitor->company->company_name
+                                'body'  => 'Your request business matching confirmed by '.$eventExhibitor->company->company_name
                             ];
                         }
                         else{
                             $notification = [
                                 'title' => 'Business Matching Confirm',
-                                'body'  => 'Your request confirmed by '.$eventExhibitor->company->company_name.' because '.$request->reason
+                                'body'  => 'Your request business matching confirmed by '.$eventExhibitor->company->company_name.' because '.$request->reason
                             ];
                         }
                         
@@ -360,13 +360,13 @@ class BusinessMatchingController extends Controller
                         if($request->reason == null || $request->reason == ''){
                             $notification = [
                                 'title' => 'Business Matching Decline',
-                                'body'  => 'Your request declined by '.$eventExhibitor->company->company_name,
+                                'body'  => 'Your request business matching declined by '.$eventExhibitor->company->company_name,
                             ];
                         }
                         else{
                             $notification = [
                                 'title' => 'Business Matching Decline',
-                                'body'  => 'Your request declined by '.$eventExhibitor->company->company_name.' because '.$request->reason,
+                                'body'  => 'Your request business matching declined by '.$eventExhibitor->company->company_name.' because '.$request->reason,
                             ];
                         }
                         
